c0620c7f49c2e095340fb043af873fe95f2ff702
Anil <anilterapalli@gmail.com>
2024-03-19 21:14:51 +0530

    msg1

--- src/main/java/com/example/testing/CustomerController.java
+++ src/main/java/com/example/testing/CustomerController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-
     @Autowired
     CustomerService customerService;
 
